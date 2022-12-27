ec061f8b2f706cbcd9e9110d335ca6547cf39722
jma <112982537+jmjra@users.noreply.github.com>
2022-12-27 20:13:55 +0100

    Update index.py

--- src/index.py
+++ src/index.py
@@ -30,6 +30,7 @@ def fn_cumul(event):
     _temp = "Passage par cumul"
     # js.alert(_temp)
     ut.affiche(message, df.shape)
+    ut.affiche(donnees, df.sample(10))
     
 def fn_semaine(event):
     _temp = "Passage par semaine"
