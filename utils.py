f7eb4b28cceff6b0c7da73506e69cad23560af8e
jma <112982537+jmjra@users.noreply.github.com>
2023-01-18 23:07:01 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,7 +13,9 @@ def affiche(id, data):
 
 def recup_donnees():
 
-    DONNEES = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQBmY_1b2XT94E60Ma_PcEVQcuonGk6r9DR-oXNB2KhrmoQtoJRfkjuqzN-w1XR8HXN0j3h_JLYyqUm/pub?gid=0&single=true&output=csv"
+    DONNEES = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQsZuVNcQd-bEICzpQsE0GOGPXN8sUHwoq920DVFpheNqDgIsMA9kkPPf1-tLh6YK64Amn_65GaljoH/pub?gid=0&single=true&output=csv"
+    
+    #DONNEES = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQBmY_1b2XT94E60Ma_PcEVQcuonGk6r9DR-oXNB2KhrmoQtoJRfkjuqzN-w1XR8HXN0j3h_JLYyqUm/pub?gid=0&single=true&output=csv"
     _data = open_url(DONNEES)
 
     df = pd.read_csv(_data)
