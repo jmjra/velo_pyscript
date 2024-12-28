8cbc2618087065f987cedd2c61a8968d28e988b8
jma <112982537+jmjra@users.noreply.github.com>
2024-12-28 22:24:36 +0100

    Update index.py
    affichage des courbes avec display

--- src/index.py
+++ src/index.py
@@ -70,8 +70,8 @@ def fn_semaine(event):
     ax = plt.stem(_x,_y)
     ut.affiche(message, _temp)
 
-    #display(fig, target="donnees")
-    ut.affiche(donnees, fig)
+    display(fig, target="donnees")
+    #ut.affiche(donnees, fig)
 
 
 def fn_mois(event):
@@ -86,8 +86,8 @@ def fn_mois(event):
     ax = plt.bar(_x,df_mois, width=0.8, align="center")
 
     #ut.affiche(donnees, df_mois[2])
-    #display(fig, target="donnees")
-    ut.affiche(donnees, fig)
+    display(fig, target="donnees")
+    #ut.affiche(donnees, fig)
 
 def fn_tableau(event):
     _temp = "Détail des sorties"
